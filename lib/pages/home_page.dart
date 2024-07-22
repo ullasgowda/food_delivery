a545a2296342916ee7eeae9275079c8a0b9047fd
Ullas Gowda <ullasgowda42@gmail.com>
2024-07-22 12:05:29 +0530

    feat: food and restaurant (#2)

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,16 @@
+import 'dart:convert';
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:food_delivery/components/custom_appbar.dart';
+import 'package:food_delivery/components/custom_description_box.dart';
+import 'package:food_delivery/components/custom_food_tile.dart';
 import 'package:food_delivery/components/custom_location_input.dart';
+import 'package:food_delivery/components/custom_tab_bar.dart';
 import 'package:food_delivery/components/drawer.dart';
+import 'package:food_delivery/models/food.dart';
+import 'package:food_delivery/models/restaurant.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,33 +19,74 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _tabController =
+        TabController(length: FoodCategory.values.length, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  List<Food> _filterMenuByCategory(FoodCategory category, List<Food> fullMenu) {
+    return fullMenu.where((food) => food.category == category).toList();
+  }
+
+  List<Widget> getFoodInThisCategory(List<Food> fullMenu) {
+    return FoodCategory.values.map((category) {
+      List<Food> categoryMenu = _filterMenuByCategory(category, fullMenu);
+
+      return ListView.builder(
+          itemCount: categoryMenu.length,
+          physics: const NeverScrollableScrollPhysics(),
+          itemBuilder: (context, index) {
+            final food = categoryMenu[index];
+
+            return CustomFoodTile(food: food, onTap: () {});
+          });
+    }).toList();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
-      drawer: CustomDrawer(),
+      drawer: const CustomDrawer(),
       body: NestedScrollView(
-        headerSliverBuilder: (context, innerBoxIsScrolled) => [
-          CustomAppBar(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Divider(
-                  indent: 25,
-                  endIndent: 25,
-                  color: Theme.of(context).colorScheme.secondary,
-                ),
-                CustomLocationInput()
+          headerSliverBuilder: (context, innerBoxIsScrolled) => [
+                CustomAppBar(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Divider(
+                          indent: 25,
+                          endIndent: 25,
+                          color: Theme.of(context).colorScheme.secondary,
+                        ),
+                        CustomLocationInput(),
+                        CustomDescriptionBox()
+                      ],
+                    ),
+                    title: CustomTabBar(
+                      tabController: _tabController,
+                    )),
               ],
+          body: Consumer<Restaurant>(
+            builder: (context, restaurant, child) => TabBarView(
+              controller: _tabController,
+              children: getFoodInThisCategory(restaurant.menu),
             ),
-            title: Text("Hi"),
-          ),
-        ],
-        body: Container(
-          color: Colors.blue,
-        ),
-      ),
+          )),
     );
   }
 }
