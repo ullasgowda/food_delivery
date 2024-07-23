906f19b1f31d592cc9fd719cc73d6630e3df9871
Ullas Gowda <ullasgowda42@gmail.com>
2024-07-23 10:33:15 +0530

    Feat/food (#3)
    Food Details page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,6 +10,7 @@ import 'package:food_delivery/components/custom_tab_bar.dart';
 import 'package:food_delivery/components/drawer.dart';
 import 'package:food_delivery/models/food.dart';
 import 'package:food_delivery/models/restaurant.dart';
+import 'package:food_delivery/pages/food_page.dart';
 import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
@@ -52,7 +53,16 @@ class _HomePageState extends State<HomePage>
           itemBuilder: (context, index) {
             final food = categoryMenu[index];
 
-            return CustomFoodTile(food: food, onTap: () {});
+            return CustomFoodTile(
+                food: food,
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => FoodPage(food: food),
+                    ),
+                  );
+                });
           });
     }).toList();
   }
